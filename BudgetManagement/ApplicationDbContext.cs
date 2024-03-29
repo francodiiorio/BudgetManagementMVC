4a65ccb1a54f41a564d3c164866a49f2fd4c5b66
franco di iorio <franco.diiorio5@gmail.com>
2024-03-28 23:44:58 -0300

    Update ApplicationDbContext.cd-create table

--- BudgetManagement/ApplicationDbContext.cs
+++ BudgetManagement/ApplicationDbContext.cs
@@ -1,6 +1,11 @@
-﻿namespace BudgetManagement
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudgetManagement
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
---
+++ BudgetManagement/Models/Expense.cs
@@ -0,0 +1,8 @@
+﻿namespace BudgetManagement.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
